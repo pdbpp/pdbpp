317d3598076127e8212d03a8524499a4813d2009
Marc Abramowitz <marc@marc-abramowitz.com>
2014-02-10 22:12:55 -0800

    Fix 3 failing tests; make all tests pass

--- testing/test_pdb.py
+++ testing/test_pdb.py
@@ -5,6 +5,7 @@ import sys
 import re
 from cStringIO import StringIO
 import py
+import pytest
 
 # make sure that we are really importing our pdb
 sys.modules.pop('pdb', None)
@@ -399,6 +400,13 @@ def test_exception_lineno():
             xpm()
 
     check(fn, """
+Traceback (most recent call last):
+.*File.*test_pdb.py.*, line NUM, in fn
+    bar()
+.*File.*test_pdb.py.*, line NUM, in bar
+    assert False
+AssertionError: assert False
+
 [NUM] > .*bar()
 -> assert False
 # u
@@ -447,6 +455,13 @@ def test_exception_through_generator():
             xpm()
 
     check(fn, """
+Traceback (most recent call last):
+.*test_pdb.py.*, line NUM, in fn
+.*for i in gen():
+.*test_pdb.py.*, line NUM, in gen
+.*assert False
+AssertionError: assert False
+
 [NUM] > .*gen()
 -> assert False
 # u
@@ -961,6 +976,8 @@ False
 """)
 
 def test_track_with_no_args():
+    pytest.importorskip('rpython.translator.tool.reftracker')
+
     def fn():
         set_trace()
         return 42
