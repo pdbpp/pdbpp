22c32b5c2f331d726504b494e49dc375275a5cbd
Marc Abramowitz <marc@marc-abramowitz.com>
2014-02-27 10:58:47 -0800

    test_pdb.py: Use next(lines) instead of lines.next()
    for Python 3 compatibility

--- testing/test_pdb.py
+++ testing/test_pdb.py
@@ -19,7 +19,7 @@ class FakeStdin:
 
     def readline(self):
         try:
-            line = self.lines.next() + '\n'
+            line = next(self.lines) + '\n'
             sys.stdout.write(line)
             return line
         except StopIteration:
