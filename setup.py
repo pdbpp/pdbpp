f7536e3bc14f7c1b5cdea38239df29c438acbaf5
Marc Abramowitz <marc@marc-abramowitz.com>
2015-03-06 15:13:04 -0800

    Bump version to 0.8.1 and update CHANGELOG

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = open(readme).read() + '\n\n' + open(changelog).read()
 
 setup(
     name='pdbpp',
-    version='0.8',
+    version='0.8.1',
     author='Antonio Cuni',
     author_email='anto.cuni@gmail.com',
     py_modules=['pdb'],
