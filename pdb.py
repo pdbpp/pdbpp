a87980ee521821a6a594243c55031949897c62c0
Antonio Cuni <anto.cuni@gmail.com>
2015-06-30 14:44:32 +0200

    use colorama to get this info, it's cleaner and hopefully works also on win7

--- pdb.py
+++ pdb.py
@@ -768,23 +768,11 @@ Frames can marked as hidden in the following ways:
         return width, height
 
     def get_terminal_size_windows(self):
-        import struct
-        from ctypes import windll, create_string_buffer
-        # stdin handle is -10
-        # stdout handle is -11
-        # stderr handle is -12
-        h = windll.kernel32.GetStdHandle(-12)
-        csbi = create_string_buffer(22)
-        res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
-        if res:
-            (bufx, bufy, curx, cury, wattr,
-             left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
-            width = right - left
-            height = bottom - top 
-        else:
-            width, height = 80, 25 # can't determine actual size - return default values
-        print 'term size', width, height
-        return width, height
+        from colorama import win32
+        info = win32.GetConsoleScreenBufferInfo(win32.STDOUT)
+        width = info.dwMaximumWindowSize.X
+        height = info.dwMaximumWindowSize.Y
+        return width-1, height
 
     def _open_editor(self, editor, lineno, filename):
         filename = filename.replace("'", "\\'")
