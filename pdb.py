c96170e92b221d0f119e91ef188ae2d235f99b4b
Antonio Cuni <anto.cuni@gmail.com>
2015-06-30 12:26:57 +0200

    return the correct term size on windows

--- pdb.py
+++ pdb.py
@@ -20,6 +20,7 @@ import traceback
 import subprocess
 import pprint
 import re
+import bdb
 from fancycompleter import Completer, ConfigurableClass, Color
 import fancycompleter
 
@@ -746,6 +747,12 @@ Frames can marked as hidden in the following ways:
     do_d = do_down
 
     def get_terminal_size(self):
+        if sys.platform == 'win32':
+            return self.get_terminal_size_windows()
+        else:
+            return self.get_terminal_size_unix()
+
+    def get_terminal_size_unix(self):
         try:
             import termios, fcntl, struct
             call = fcntl.ioctl(0, termios.TIOCGWINSZ, "\x00"*8)
@@ -758,6 +765,25 @@ Frames can marked as hidden in the following ways:
         # Work around above returning width, height = 0, 0 in Emacs
         width = width if width != 0 else 80
         height = height if height != 0 else 24
+        return width, height
+
+    def get_terminal_size_windows(self):
+        import struct
+        from ctypes import windll, create_string_buffer
+        # stdin handle is -10
+        # stdout handle is -11
+        # stderr handle is -12
+        h = windll.kernel32.GetStdHandle(-12)
+        csbi = create_string_buffer(22)
+        res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+        if res:
+            (bufx, bufy, curx, cury, wattr,
+             left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+            width = right - left
+            height = bottom - top 
+        else:
+            width, height = 80, 25 # can't determine actual size - return default values
+        print 'term size', width, height
         return width, height
 
     def _open_editor(self, editor, lineno, filename):
