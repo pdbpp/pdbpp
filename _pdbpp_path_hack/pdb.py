6ad13d9c7273303678656827811d35b95290b252
Daniel Hahler <github@thequod.de>
2018-11-04 18:16:48 +0100

    _pdbpp_path_hack/pdb.py: fix ResourceWarning (#97)
    Fixes:
    
    > …/.tox/venvs/py36-django21/lib/python3.6/site-packages/_pdbpp_path_hack/pdb.py:4:
    > ResourceWarning: unclosed file <_io.TextIOWrapper
    > name='…/.tox/venvs/py36-django21/lib/python3.6/site-packages/pdb.py'
    > mode='r' encoding='UTF-8'>
      os.path.dirname(os.path.dirname(__file__)), 'pdb.py')).read(),
      os.path.join(

--- _pdbpp_path_hack/pdb.py
+++ _pdbpp_path_hack/pdb.py
@@ -1,5 +1,5 @@
 # this file is needed to hijack pdb without eggs
 import os.path
-exec(compile(open(os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), 'pdb.py')).read(), os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), 'pdb.py'), 'exec'))
+pdb_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pdb.py')
+with open(pdb_path) as f:
+    exec(compile(f.read(), pdb_path, 'exec'))
