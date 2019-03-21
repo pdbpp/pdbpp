328ce3e448ef6c55395f7fec8ba694dd22a01819
Daniel Hahler <git@thequod.de>
2019-03-21 21:37:16 +0100

    fixup! fixup! fixup! fixup! tests: restore sys.gettrace after test

--- testing/test_pdb.py
+++ testing/test_pdb.py
@@ -21,20 +21,21 @@ import pdb  # noqa: E402
 _orig_trace = sys.gettrace()
 
 
-@pytest.fixture(autouse=True)
-def restore_settrace():
-    """(Re)store sys.gettrace after test run.
+if _orig_trace:
+    @pytest.fixture(autouse=True)
+    def restore_settrace():
+        """(Re)store sys.gettrace after test run.
 
-    This is required to re-enable coverage tracking.
-    """
-    if sys.gettrace() is _orig_trace:
+        This is required to re-enable coverage tracking.
+        """
+        assert sys.gettrace() is _orig_trace
+
         yield
+
         newtrace = sys.gettrace()
         if newtrace is not _orig_trace:
             assert newtrace is None
             sys.settrace(_orig_trace)
-    else:
-        yield
 
 
 class FakeStdin:
