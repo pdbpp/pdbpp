9e79a4897aebc4968b18a9bc8a3f19a8a444a25d
Daniel Hahler <github@thequod.de>
2019-03-27 23:17:42 +0100

    tests: fix put tests on py27 with user pdbrc (#178)

--- testing/conftest.py
+++ testing/conftest.py
@@ -26,3 +26,18 @@ def restore_settrace():
     if newtrace is not _orig_trace:
         assert newtrace is None
         sys.settrace(_orig_trace)
+
+
+@pytest.fixture
+def tmphome(tmpdir, monkeypatch):
+    """Set up HOME in a temporary directory.
+
+    This ignores any real ~/.pdbrc.py then, and seems to be
+    required also with linecache on py27, where it would read contents from
+    ~/.pdbrc?!.
+    """
+    monkeypatch.setenv("HOME", str(tmpdir))
+    monkeypatch.setenv("USERPROFILE", str(tmpdir))
+
+    with tmpdir.as_cwd():
+        yield tmpdir
