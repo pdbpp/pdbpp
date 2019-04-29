2785e0435b707e3946c87f671f31aad81820b55c
Daniel Hahler <git@thequod.de>
2019-04-29 05:28:57 +0200

    Add support for PDBPP_HIJACK_PDB=0 to not replace pdb module (#134)

--- _pdbpp_path_hack/pdb.py
+++ _pdbpp_path_hack/pdb.py
@@ -1,5 +1,13 @@
 # this file is needed to hijack pdb without eggs
-import os.path
-pdb_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pdb.py')
+import os
+
+if int(os.environ.get('PDBPP_HIJACK_PDB', 1)):
+    pdb_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pdb.py')
+else:
+    # Use original pdb.
+    import code  # arbitrary module which stays in the same dir as pdb
+
+    pdb_path = os.path.join(os.path.dirname(code.__file__), 'pdb.py')
+
 with open(pdb_path) as f:
     exec(compile(f.read(), pdb_path, 'exec'))
