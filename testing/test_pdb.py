pdbpp/pdbpp

commit a88be00d31f1ff38e26711a1d99589d830524c9e

do_list: optimize for no highlighting, return

pdb.py
@@ -711,9 +711,12 @@ except for when using the function decorator.
     do_ll = do_longlist
 
     def do_list(self, arg):
+        if not self.config.highlight:
+            return super(Pdb, self).do_list(arg)
+
         oldstdout = self.stdout
         self.stdout = StringIO()
-        super(Pdb, self).do_list(arg)
+        ret = super(Pdb, self).do_list(arg)
         orig_pdb_lines = self.stdout.getvalue().splitlines()
         self.stdout = oldstdout
 
@@ -729,6 +732,7 @@ except for when using the function decorator.
         ).splitlines()
         for prefix, src in zip(prefixes, formatted_src_lines):
             print("%s\t%s" % (prefix, src), file=self.stdout)
+        return ret
 
     do_list.__doc__ = pdb.Pdb.do_list.__doc__
     do_l = do_list

commit efee015978b6a7a8b3e6614031e874dcdf0ae679

do_list: fix, and use _print_lines with Python 3.2+
Fixes a regression introduced in a88be00 (not released).

pdb.py
@@ -720,32 +720,56 @@ except for when using the function decorator.
 
     do_ll = do_longlist
 
-    def do_list(self, arg):
-        if not self.config.highlight:
-            return super(Pdb, self).do_list(arg)
+    if sys.version_info >= (3, 2):
+        def _print_lines(self, lines, *args, **kwargs):
+            """Enhance original _print_lines with highlighting.
 
-        oldstdout = self.stdout
-        self.stdout = StringIO()
-        ret = super(Pdb, self).do_list(arg)
-        orig_pdb_lines = self.stdout.getvalue().splitlines()
-        self.stdout = oldstdout
+            Used via do_list currently only, do_source and do_longlist are
+            overridden.
+            """
+            if self.config.highlight and self.config.use_pygments:
+                if lines:
+                    lines = self.format_source(
+                        "".join(lines)
+                    ).splitlines(keepends=True)
 
-        # Format source without prefixes added by pdb, including line numbers.
-        prefixes = []
-        src_lines = []
-        for x in orig_pdb_lines:
-            prefix, src = x.split('\t', 1)
-            prefixes.append(prefix)
-            src_lines.append(src)
-        formatted_src_lines = self.format_source(
-            "\n".join(src_lines)
-        ).splitlines()
-        for prefix, src in zip(prefixes, formatted_src_lines):
-            print("%s\t%s" % (prefix, src), file=self.stdout)
-        return ret
+            return super(Pdb, self)._print_lines(lines, *args, **kwargs)
+    else:
+        # Only for Python 2.7, where _print_lines is not used/available.
+        def do_list(self, arg):
+            if not self.config.highlight or not self.config.use_pygments:
+                return super(Pdb, self).do_list(arg)
 
-    do_list.__doc__ = pdb.Pdb.do_list.__doc__
-    do_l = do_list
+            oldstdout = self.stdout
+            self.stdout = StringIO()
+            ret = super(Pdb, self).do_list(arg)
+            orig_pdb_lines = self.stdout.getvalue().splitlines()
+            self.stdout = oldstdout
+
+            # Format source without prefixes added by pdb, including line numbers.
+            prefixes = []
+            src_lines = []
+            if not orig_pdb_lines:
+                return
+
+            for x in orig_pdb_lines:
+                prefix, _, src = x.partition('\t')
+                prefixes.append(prefix)
+                src_lines.append(src)
+            formatted_src_lines = self.format_source(
+                "\n".join(src_lines) + "\n"
+            ).splitlines()
+
+            assert len(formatted_src_lines) == len(src_lines), (
+                repr([formatted_src_lines, src_lines])
+            )
+
+            for prefix, src in zip(prefixes, formatted_src_lines):
+                print("%s\t%s" % (prefix, src), file=self.stdout)
+            return ret
+
+        do_list.__doc__ = pdb.Pdb.do_list.__doc__
+        do_l = do_list
 
     def do_continue(self, arg):
         if arg != '':

testing/test_pdb.py
@@ -47,6 +47,10 @@ class ConfigWithHighlight(ConfigTest):
     highlight = True
 
 
+class ConfigWithPygments(ConfigWithHighlight):
+    use_pygments = True
+
+
 class PdbTest(pdb.Pdb):
     use_rawinput = 1
 
@@ -795,6 +799,45 @@ NUM  ->	        return a
 """.format(line_num=fn.__code__.co_firstlineno))
 
 
+def test_shortlist_with_highlight_and_EOF():
+    def fn():
+        a = 1
+        set_trace(Config=ConfigWithPygments)
+        return a
+
+    check(fn, """
+[NUM] > .*fn()
+-> return a
+   5 frames hidden .*
+# l {line_num}, 3
+[EOF]
+# c
+""".format(line_num=100000))
+
+
+def test_shortlist_with_highlight(monkeypatch):
+    monkeypatch.setenv("TERM", "xterm-256color")
+
+    def fn():
+        a = 1
+        set_trace(Config=ConfigWithPygments)
+
+        return a
+
+    check(fn, """
+[NUM] > .*fn()
+-> return a
+   5 frames hidden .*
+# l {line_num}, 4
+NUM  \t    ^[[38;5;28;01mdef^[[39;00m ^[[38;5;21mfn^[[39m():
+NUM  \t        a ^[[38;5;241m=^[[39m ^[[38;5;241m1^[[39m
+NUM  \t        set_trace(Config^[[38;5;241m=^[[39mConfigWithPygments)
+NUM  \t$
+NUM  ->\t        ^[[38;5;28;01mreturn^[[39;00m a
+# c
+""".format(line_num=fn.__code__.co_firstlineno))
+
+
 def test_shortlist_without_arg():
     """Ensure that forget was called for lineno."""
     def fn():
@@ -977,7 +1020,7 @@ def test_sticky_range():
 <CLEARSCREEN>
 >.*
 
- %d             set_trace()
+%d \\s+         set_trace()
 NUM  ->         a = 1
 NUM             b = 2
 # c