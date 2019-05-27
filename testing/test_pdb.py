293134007896ca81c25615448cd31231d372fffd
Daniel Hahler <git@thequod.de>
2019-05-28 00:24:13 +0200

    tests: test_complete_removes_duplicates_with_coloring (#20)

--- testing/test_pdb.py
+++ testing/test_pdb.py
@@ -3170,6 +3170,43 @@ True
 """ % lineno)
 
 
+def test_complete_removes_duplicates_with_coloring(
+    monkeypatch_readline, readline_param
+):
+    def fn():
+        helpvar = 42  # noqa: F841
+
+        def check_completions():
+            # Patch readline to return expected results for "help".
+            monkeypatch_readline("help", 0, 4)
+
+            if readline_param == "pyrepl":
+                assert pdb.local.GLOBAL_PDB.fancycompleter.config.use_colors is True
+                assert get_completions("help") == [
+                    "\x1b[000;00m\x1b[00mhelp\x1b[00m",
+                    "\x1b[001;00m\x1b[33;01mhelpvar\x1b[00m",
+                    " ",
+                ]
+            else:
+                assert pdb.local.GLOBAL_PDB.fancycompleter.config.use_colors is False
+                assert get_completions("help") == ["help", "helpvar"]
+            return True
+
+        set_trace()
+
+    _, lineno = inspect.getsourcelines(fn)
+
+    check(fn, """
+--Return--
+[NUM] > .*fn()->None
+.*
+   5 frames hidden .*
+# check_completions()
+True
+# c
+""")
+
+
 @pytest.mark.skipif(sys.version_info < (3, ), reason="py2: no completion for break")
 def test_completion_removes_tab_from_fancycompleter(monkeypatch_readline):
     def fn():
