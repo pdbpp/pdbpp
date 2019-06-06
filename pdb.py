2fc690c6894ba36d4f2cfb8d738e324073646f18
Daniel Hahler <git@thequod.de>
2019-06-06 15:42:33 +0200

    fixup! Fix hideframe decorator for Python 3.8

--- pdb.py
+++ pdb.py
@@ -1593,6 +1593,15 @@ def hideframe(func):
             c.co_names, c.co_varnames, c.co_filename,
             c.co_name, c.co_firstlineno, c.co_lnotab,
             c.co_freevars, c.co_cellvars)
+    else:
+        # Python 3 takes an additional arg -- kwonlyargcount.
+        c = types.CodeType(
+            c.co_argcount, c.co_kwonlyargcount, c.co_nlocals, c.co_stacksize,
+            c.co_flags, c.co_code,
+            c.co_consts + (_HIDE_FRAME,),
+            c.co_names, c.co_varnames, c.co_filename,
+            c.co_name, c.co_firstlineno, c.co_lnotab,
+            c.co_freevars, c.co_cellvars)
 
     func.__code__ = c
     return func
