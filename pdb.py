80483971c89fa985b1ea063f9f7a1aa2a88baa1e
Daniel Hahler <git@thequod.de>
2019-08-17 00:06:19 +0200

    _setup_color_stream: colorama.AnsiToWin32: convert=True, strip=False

--- pdb.py
+++ pdb.py
@@ -298,7 +298,8 @@ class Pdb(pdb.Pdb, ConfigurableClass, object):
 
         if os.name == 'nt' and supports_color:
             if self.config.highlight or self.config.use_pygments is not False:
-                stream = colorama.AnsiToWin32(stream).stream
+                # Wrap stream with colorama, skipping "isatty" check.
+                stream = colorama.AnsiToWin32(stream, convert=True, strip=False).stream
         return stream
 
     def _disable_pytest_capture_maybe(self):
