357303ca9642ccb27c3d4644a9902840c16cca37
Daniel Hahler <git@thequod.de>
2019-10-05 15:49:49 +0200

    test_integration: use readline_param

--- testing/test_pdb.py
+++ testing/test_pdb.py
@@ -3582,7 +3582,7 @@ True
 """)
 
 
-def test_integration(testdir, tmphome):
+def test_integration(testdir, tmphome, readline_param):
     """Integration test."""
     import sys
 
@@ -3595,12 +3595,18 @@ def test_integration(testdir, tmphome):
     child = testdir.spawn(sys.executable + " " + str(f), expect_timeout=1)
 
     # NOTE: b'\x1b[?12l\x1b[?25h' comes via pyrepl.
-    pdbpp_prompt = "\n(Pdb++) \x1b[?12l\x1b[?25h"
+    if readline_param == "pyrepl":
+        pdbpp_prompt = "\n(Pdb++) \x1b[?12l\x1b[?25h"
+    else:
+        pdbpp_prompt = "\n(Pdb++) "
     child.expect_exact(pdbpp_prompt)
 
     # Completes help as unique (coming from pdb and fancycompleter).
     child.send(b"hel\t")
-    child.expect_exact(b"\x1b[1@h\x1b[1@e\x1b[1@l\x1b[1@p")
+    if readline_param == "pyrepl":
+        child.expect_exact(b"\x1b[1@h\x1b[1@e\x1b[1@l\x1b[1@p")
+    else:
+        child.expect_exact(b"help")
     child.sendline("")
     child.expect_exact("\r\nDocumented commands")
     child.expect_exact(pdbpp_prompt)
@@ -3609,16 +3615,26 @@ def test_integration(testdir, tmphome):
     # fancycompleter.
     if sys.version_info >= (3, 3):
         child.send(b"b \t")
-        child.expect_exact(b'\x1b[1@b\x1b[1@ \x1b[?25ltest_file.py:\x1b[?12l\x1b[?25h')
+        if readline_param == "pyrepl":
+            child.expect_exact(b'\x1b[1@b\x1b[1@ \x1b[?25ltest_file.py:\x1b[?12l\x1b[?25h')
+        else:
+            child.expect_exact(b'b test_file.py:')
+
         child.sendline("")
-        child.expect_exact(
-            b"\x1b[23D\r\n\r\x1b[?1l\x1b>*** Bad lineno: \r\n"
-            b"\x1b[?1h\x1b=\x1b[?25l\x1b[1A\r\n(Pdb++) \x1b[?12l\x1b[?25h"
-        )
+        if readline_param == "pyrepl":
+            child.expect_exact(
+                b"\x1b[23D\r\n\r\x1b[?1l\x1b>*** Bad lineno: \r\n"
+                b"\x1b[?1h\x1b=\x1b[?25l\x1b[1A\r\n(Pdb++) \x1b[?12l\x1b[?25h"
+            )
+        else:
+            child.expect_exact(b"\r\n*** Bad lineno: \r\n(Pdb++) ")
 
     child.sendline("c")
     rest = child.read()
-    assert rest == b'\x1b[1@c\x1b[9D\r\n\r\x1b[?1l\x1b>'
+    if readline_param == "pyrepl":
+        assert rest == b'\x1b[1@c\x1b[9D\r\n\r\x1b[?1l\x1b>'
+    else:
+        assert rest == b'c\r\n'
 
 
 def test_complete_with_bang(monkeypatch_readline):
