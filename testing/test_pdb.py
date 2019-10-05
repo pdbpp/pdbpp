pdbpp/pdbpp

commit 5e7d16aef3e62c9b65a3c425a69e3c5d4cb3dadd

Format source before truncating it
This is important to have code highlighting correct for when truncating
in the middle of a token.

pdb.py
@@ -911,15 +911,55 @@ except for when using the function decorator.
             lineno = start
         self._print_lines_pdbpp(lines, lineno)
 
+    @staticmethod
+    def _truncate_to_visible_length(s, maxlength):
+        """Truncate string to visible length (with escape sequences ignored)."""
+        matches = list(RE_COLOR_ESCAPES.finditer(s))
+        if not matches:
+            return s[:maxlength]
+
+        ret = ""
+        total_visible_len = 0
+        pos = 0
+        for m in matches:
+            m_start = m.regs[0][0]
+            m_end = m.regs[0][1]
+            add_visible = s[pos:m_start]
+            len_visible = m_start - pos
+            overflow = (len_visible + total_visible_len) - maxlength
+            if overflow >= 0:
+                if overflow == 0:
+                    ret += add_visible
+                else:
+                    ret += add_visible[:-overflow]
+                ret += s[m_start:m_end]
+                return ret
+            total_visible_len += len_visible
+            ret += add_visible
+            ret += s[m_start:m_end]
+            pos = m_end
+        else:
+            assert maxlength - total_visible_len > 0
+            rest = s[m_end:]
+            ret += rest[:maxlength - total_visible_len]
+
+        assert len(RE_COLOR_ESCAPES.sub("", ret)) <= maxlength
+        return ret
+
     def _print_lines_pdbpp(self, lines, lineno, print_markers=True):
         lines = [line[:-1] for line in lines]  # remove the trailing '\n'
         lines = [line.replace('\t', '    ')
                  for line in lines]  # force tabs to 4 spaces
         width, height = self.get_terminal_size()
 
+        if self.config.use_pygments is not False:
+            src = self.format_source('\n'.join(lines))
+            lines = src.splitlines()
+
         if self.config.truncate_long_lines:
             maxlength = max(width - 9, 16)
-            lines = [line[:maxlength] for line in lines]
+            lines = [self._truncate_to_visible_length(line, maxlength)
+                     for line in lines]
         else:
             maxlength = max(map(len, lines))
 
@@ -939,10 +979,6 @@ except for when using the function decorator.
             # Fill with spaces.  This is important for setbgcolor, although
             # only for the current/marked line really.
             lines = [line.ljust(maxlength) for line in lines]
-
-        if self.config.use_pygments is not False:
-            src = self.format_source('\n'.join(lines))
-            lines = src.splitlines()
 
         lineno_width = len(str(lineno + len(lines)))
         if print_markers:

testing/test_pdb.py
@@ -1285,6 +1285,40 @@ NUM +->\t        ^[[38;5;28;01mreturn^[[39;00m a
 """.format(line_num=fn.__code__.co_firstlineno - 1))
 
 
+def test_truncated_source_with_pygments():
+
+    def fn():
+        """some docstring longer than maxlength for truncate_long_lines, which is 80"""
+        a = 1
+        set_trace(Config=ConfigWithPygments)
+
+        return a
+
+    check(fn, """
+[NUM] > .*fn()
+-> ^[[38;5;28;01mreturn^[[39;00m a
+   5 frames hidden .*
+# l {line_num}, 5
+NUM +\t$
+NUM +\t    ^[[38;5;28;01mdef^[[39;00m ^[[38;5;21mfn^[[39m():
+NUM +\t        ^[[38;5;124m\"\"\"some docstring longer than maxlength for truncate_long_lines, which is 80\"\"\"^[[39m
+NUM +\t        a ^[[38;5;241m=^[[39m ^[[38;5;241m1^[[39m
+NUM +\t        set_trace(Config^[[38;5;241m=^[[39mConfigWithPygments)
+NUM +\t$
+# sticky
+<CLEARSCREEN>
+>.*
+
+NUM +       ^[[38;5;28;01mdef^[[39;00m ^[[38;5;21mfn^[[39m():
+NUM +           ^[[38;5;124m\"\"\"some docstring longer than maxlength for truncate_long_lines^[[39m
+NUM +           a ^[[38;5;241m=^[[39m ^[[38;5;241m1^[[39m
+NUM +           set_trace(Config^[[38;5;241m=^[[39mConfigWithPygments)
+NUM +$
+NUM +->         ^[[38;5;28;01mreturn^[[39;00m a
+# c
+""".format(line_num=fn.__code__.co_firstlineno - 1))  # noqa: E501
+
+
 def test_shortlist_with_highlight():
 
     def fn():
@@ -4508,3 +4542,19 @@ def test_only_question_mark(monkeypatch):
 do_help
 # c
 """)
+
+
+@pytest.mark.parametrize('s,maxlength,expected', [
+    ('foo', 3, 'foo'),
+    ('foo', 1, 'f'),
+
+    # Keeps trailing escape sequences (for reset at least).
+    ("\x1b[39m1\x1b[39m23", 1, "\x1b[39m1\x1b[39m"),
+    ("\x1b[39m1\x1b[39m23", 2, "\x1b[39m1\x1b[39m2"),
+    ("\x1b[39m1\x1b[39m23", 3, "\x1b[39m1\x1b[39m23"),
+    ("\x1b[39m1\x1b[39m23", 100, "\x1b[39m1\x1b[39m23"),
+
+    ("\x1b[39m1\x1b[39m", 100, "\x1b[39m1\x1b[39m"),
+])
+def test_truncate_to_visible_length(s, maxlength, expected):
+    assert pdb.Pdb._truncate_to_visible_length(s, maxlength) == expected

commit 84a6373f8e5307f11f36482f59eb31627b58073a

Fix filling current line with spaces (fixup)
Regressed in 5e7d16a.

pdb.py
@@ -670,9 +670,6 @@ class Pdb(pdb.Pdb, ConfigurableClass, object):
         if self.config.highlight:
             lineno = Color.set(self.config.line_number_color, lineno)
         line = '%s  %2s %s' % (lineno, marker, line)
-        if self.config.highlight and marker == '->':
-            if self.config.current_line_color:
-                line = setbgcolor(line, self.config.current_line_color)
         return line
 
     def execRcLines(self):
@@ -976,11 +973,6 @@ except for when using the function decorator.
                         lines = lines[:maxlines]
                         lines.append('...')
 
-        if self.config.highlight and print_markers:
-            # Fill with spaces.  This is important for setbgcolor, although
-            # only for the current/marked line really.
-            lines = [line.ljust(maxlength) for line in lines]
-
         lineno_width = len(str(lineno + len(lines)))
         if print_markers:
             for i, line in enumerate(lines):
@@ -990,7 +982,16 @@ except for when using the function decorator.
                     marker = '>>'
                 else:
                     marker = ''
-                lines[i] = self._format_line(lineno, marker, line, lineno_width)
+                line = self._format_line(lineno, marker, line, lineno_width)
+
+                # Highlight current line (filled with spaces for background).
+                if self.config.highlight and marker == '->':
+                    if self.config.current_line_color:
+                        len_visible = len(RE_COLOR_ESCAPES.sub("", line))
+                        line = line + " " * (width - len_visible)
+                        line = setbgcolor(line, self.config.current_line_color)
+
+                lines[i] = line
                 lineno += 1
         else:
             for i, line in enumerate(lines):

testing/test_pdb.py
@@ -48,6 +48,7 @@ class ConfigTest(pdb.DefaultConfig):
     editor = 'emacs'
     stdin_paste = 'epaste'
     disable_pytest_capturing = False
+    current_line_color = 44
 
 
 class ConfigWithHighlight(ConfigTest):
@@ -1315,6 +1316,18 @@ NUM +           a ^[[38;5;241m=^[[39m ^[[38;5;241m1^[[39m
 NUM +           set_trace(Config^[[38;5;241m=^[[39mConfigWithPygments)
 NUM +$
 NUM +->         ^[[38;5;28;01mreturn^[[39;00m a
+# import pdb; pdb.GLOBAL_PDB.config.highlight = True
+# sticky
+# sticky
+<CLEARSCREEN>
+>.*
+
+<COLORNUM> +       ^[[38;5;28;01mdef^[[39;00m ^[[38;5;21mfn^[[39m():
+<COLORNUM> +           ^[[38;5;124m\"\"\"some docstring longer than maxlength for truncate_long_lines^[[39m
+<COLORNUM> +           a ^[[38;5;241m=^[[39m ^[[38;5;241m1^[[39m
+<COLORNUM> +           set_trace(Config^[[38;5;241m=^[[39mConfigWithPygments)
+<COLORNUM> +$
+<COLORCURLINE> +->         ^[[38;5;28;01;44mreturn^[[39;00;44m a                                                       ^[[00m
 # c
 """.format(line_num=fn.__code__.co_firstlineno - 1))  # noqa: E501
 
@@ -1453,7 +1466,7 @@ def test_longlist_with_highlight():
 <COLORNUM>         def fn():
 <COLORNUM>             a = 1
 <COLORNUM>             set_trace(Config=ConfigWithHighlight)
-^[[39;49;7m^[[36;01;39;49;7m\d+^[[00;39;49;7m  ->         return a                                                       ^[[00m
+<COLORCURLINE> +->         return a                                                       ^[[00m$
 # c
 """)  # noqa: E501
 
@@ -1682,14 +1695,12 @@ InnerTestException:
 
 def test_sticky_dunder_exception_with_highlight():
     """Test __exception__ being displayed in sticky mode."""
-    class ConfigWithCurrentLineColor(ConfigWithHighlight):
-        current_line_color = 44
 
     def fn():
         def raises():
             raise InnerTestException()
 
-        set_trace(Config=ConfigWithCurrentLineColor)
+        set_trace(Config=ConfigWithHighlight)
         raises()
 
     check(fn, """
@@ -1836,15 +1847,14 @@ NUM  ->             raise InnerTestException()
 
 
 def test_sticky_dunder_return_with_highlight():
-    class ConfigWithPygments(ConfigWithHighlight):
-        use_pygments = True
-        current_line_color = 44
+    class Config(ConfigWithHighlight, ConfigWithPygments):
+        pass
 
     def fn():
         def returns():
             return 40 + 2
 
-        set_trace(Config=ConfigWithPygments)
+        set_trace(Config=Config)
         returns()
 
     expected, lines = run_func(fn, '# s\n# sticky\n# r\n# retval\n# c')