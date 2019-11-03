f49b816ae6c81447c5f2e5cf229ad22b524a4c3f
Daniel Hahler <git@thequod.de>
2019-11-03 02:51:45 +0100

    remove test, covered by test_python_m_pdb_uses_pdbpp_and_env in general

--- testing/test_meta.py
+++
@@ -1,7 +0,0 @@
-def test_inspect_source():
-    import inspect
-    import pdb
-    import os
-
-    assert inspect.findsource(pdb.Pdb)
-    assert os.path.basename(inspect.getfile(pdb.Pdb)) == "pdbpp.py"
