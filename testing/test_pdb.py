1bbfa4f60c6376ae252f4dd7529f7eb725d1ef4f
Daniel Hahler <git@thequod.de>
2020-03-22 14:10:12 +0100

    Save "__exception__" with post mortem debugging (via main)

--- testing/test_pdb.py
+++ testing/test_pdb.py
@@ -5354,3 +5354,44 @@ is_skipped_module\? testing.test_pdb
    5 frames hidden (try 'help hidden_frames')
 # c
 """)
+
+
+def test_exception_info_main(testdir):
+    """Test that interaction adds __exception__ similar to user_exception."""
+    p1 = testdir.makepyfile(
+        """
+        def f():
+            raise ValueError("foo")
+
+        f()
+        """
+    )
+    testdir.monkeypatch.setenv("PDBPP_COLORS", "0")
+    result = testdir.run(
+        sys.executable, "-m", "pdb", "-c", "sticky", "-c", "cont", str(p1)
+    )
+    result.stdout.fnmatch_lines(
+        [
+            "1  -> def f():",
+            '2         raise ValueError("foo")',
+            '3     ',
+            '4     f()',
+            'Uncaught exception. Entering post mortem debugging',
+            "Running 'cont' or 'step' will restart the program",
+            "[[]5[]] > *test_exception_info_main.py(2)f()",
+            "",
+            "1     def f():",
+            '2  ->     raise ValueError("foo")',
+            "ValueError: foo",
+            "(Pdb++) Post mortem debugger finished."
+            " The *test_exception_info_main.py will be restarted",
+            "",
+            "[[]2[]] > *test_exception_info_main.py(1)<module>()",
+            "",
+            '1  -> def f():',
+            '2         raise ValueError("foo")',
+            '3     ',
+            '4     f()',
+            '(Pdb++) ',
+        ]
+    )
