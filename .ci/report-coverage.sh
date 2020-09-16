36cd2276ba728a99cc722f1234686160e10f0abd
Daniel Hahler <git@thequod.de>
2020-09-16 19:22:18 +0200

    .ci/report-coverage.sh: coverage-travis.xml

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -6,5 +6,5 @@ set -ex
 curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://codecov.io/bash
 
 for _ in 1 2 3 4 5; do
-  bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage.xml "$@" && break
+  bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage-travis.xml "$@" && break
 done
