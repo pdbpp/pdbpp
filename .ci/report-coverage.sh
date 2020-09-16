3679a841c519525ff7407f0f334a317875e78d12
Daniel Hahler <git@thequod.de>
2020-09-16 18:56:42 +0200

    ci: revisit coverage reporting

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
-#
-# Submit/report coverage when run from Travis for "coverage" tox factors.
 
 set -ex
 
-coverage_xml="coverage-travis.xml"
-if ! [ -f "$coverage_xml" ]; then
-  exit
-fi
+# Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
+curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://codecov.io/bash
 
-codecov_bash=/tmp/codecov-bash.sh
-
-if ! [ -f "$codecov_bash" ]; then
-  curl -sSf --retry 5 -o "$codecov_bash" https://codecov.io/bash
-  chmod +x "$codecov_bash"
-fi
-
-"$codecov_bash" -Z -X fix -f "$coverage_xml" -n "${TOX_ENV_NAME%-coverage}" -F "${CODECOV_FLAG},${TOX_ENV_NAME%-coverage}"
-rm "$coverage_xml"
+for _ in 1 2 3 4 5; do
+  bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage.xml "$@" && break
+done
