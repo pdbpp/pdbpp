020850bee720808c1f9dc83f3f9d245005cd88b7
Daniel Hahler <git@thequod.de>
2020-11-14 06:11:35 +0100

    ci: pin codecov
    Ref: https://github.com/codecov/codecov-bash/pull/373#issuecomment-727146528

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -3,7 +3,8 @@
 set -ex
 
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
-curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://codecov.io/bash
+# NOTE: use version prior to https://github.com/codecov/codecov-bash/pull/373#issuecomment-727146528
+curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://raw.githubusercontent.com/codecov/codecov-bash/6329481eb0b26ed755524738240b3e381cdfb76d/codecov
 
 for _ in 1 2 3 4 5; do
   bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage-travis.xml "$@" && break
