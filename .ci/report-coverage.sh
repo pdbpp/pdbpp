9bf94b2ac3b5ec43f01a9c56dd17c6947dfba944
Daniel Hahler <git@thequod.de>
2020-11-14 06:29:50 +0100

    TEST: pin codecov
    Ref: https://github.com/codecov/codecov-bash/pull/373#issuecomment-727146528
    
    Via https://github.com/pdbpp/pdbpp/pull/427

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -3,7 +3,8 @@
 set -ex
 
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
-curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://codecov.io/bash
+# NOTE: use version prior to https://github.com/codecov/codecov-bash/pull/373#issuecomment-727146528
+curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://raw.githubusercontent.com/codecov/codecov-bash/20201104-3e6f2fc/codecov
 
 for _ in 1 2 3 4 5; do
   bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage-travis.xml "$@" && break
