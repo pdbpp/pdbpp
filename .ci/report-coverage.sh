374d31c993d9d69d99d0d233e0797b0d4a0ed068
Daniel Hahler <git@thequod.de>
2020-11-14 06:45:28 +0100

    TEST: Revert "TEST: pin codecov"
    This reverts commit 9bf94b2ac3b5ec43f01a9c56dd17c6947dfba944.

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -3,8 +3,7 @@
 set -ex
 
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
-# NOTE: use version prior to https://github.com/codecov/codecov-bash/pull/373#issuecomment-727146528
-curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://raw.githubusercontent.com/codecov/codecov-bash/20201104-3e6f2fc/codecov
+curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://codecov.io/bash
 
 for _ in 1 2 3 4 5; do
   bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage-ci.xml "$@" && break
