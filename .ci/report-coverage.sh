b82964a52b4de3674246f8c5bcfd4b82cba53776
Daniel Hahler <git@thequod.de>
2020-11-14 06:17:20 +0100

    6329481e is broken

--- .ci/report-coverage.sh
+++ .ci/report-coverage.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Set --connect-timeout to work around https://github.com/curl/curl/issues/4461
 # NOTE: use version prior to https://github.com/codecov/codecov-bash/pull/373#issuecomment-727146528
-curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://raw.githubusercontent.com/codecov/codecov-bash/6329481eb0b26ed755524738240b3e381cdfb76d/codecov
+curl -S -L --connect-timeout 5 --retry 6 -s -o /tmp/codecov.sh https://raw.githubusercontent.com/codecov/codecov-bash/b90013320cd9d7d5c131319a48a92c39fbd6540c/codecov
 
 for _ in 1 2 3 4 5; do
   bash /tmp/codecov.sh -Z -X fix -X s3 -f coverage-travis.xml "$@" && break
