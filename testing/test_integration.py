b62a3381bd1db8fa3ffaa46579f477b187e2f37c
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2020-11-22 08:22:19 -0500

    Fix hidden_frames discrepancy with IPython (#426)

--- testing/test_integration.py
+++ testing/test_integration.py
@@ -1,5 +1,7 @@
 import sys
 
+import pytest
+
 
 def test_integration(testdir, readline_param):
     tmpdir = testdir.tmpdir
@@ -60,3 +62,25 @@ def test_integration(testdir, readline_param):
         assert rest == b'\x1b[1@c\x1b[9D\r\n\r\x1b[?1l\x1b>'
     else:
         assert rest == b'c\r\n'
+
+
+def test_ipython(testdir):
+    """Test integration when used with IPython.
+
+    - `up` used to crash due to conflicting `hidden_frames` attribute/method.
+    """
+    pytest.importorskip("IPython")
+    child = testdir.spawn(
+        "{} -m IPython --colors=nocolor --simple-prompt".format(
+            sys.executable,
+        )
+    )
+    child.sendline("%debug raise ValueError('my_value_error')")
+    child.sendline("up")
+    child.expect_exact("\r\nipdb++> ")
+    child.sendline("c")
+    child.expect_exact("\r\nValueError: my_value_error\r\n")
+    child.expect_exact("\r\nIn [2]: ")
+    child.sendeof()
+    child.sendline("y")
+    assert child.wait() == 0
